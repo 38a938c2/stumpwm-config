9c29220d0c0a786d201c5a914b3c543c8a1ca25c
Michael Raskin <38a938c2@rambler.ru>
2018-02-23 02:45:42 +0100

    Syncing from Monotone at 20180223-024542. Last commit message was trying to fix split-running; tagging additions

--- defun.lisp
+++ defun.lisp
@@ -1217,58 +1217,48 @@
      (ps (map 'string 'code-char p))
      (s
        (format
-         nil "/tmp/ff.~a/marionette-socket"
-         (cl-ppcre:regex-replace "[.]" ps "/sockets/")))
+         nil "/tmp/ff.~a/sockets/~a/marionette-socket"
+         (uiop:getenv "USER")
+         (cl-ppcre:regex-replace "[.].*" ps "")))
      )
     (when (probe-file s) s)))
 
-(defun listen-on-socket (socket-directory)
-  (let*
-    ((socket-container (format nil "~a/stumpwm-socket" socket-directory))
-     (socket-name (format nil "~a/socket" socket-container))
-     (socket (make-instance 'sb-bsd-sockets:local-socket :type :stream)))
-    (ensure-directories-exist socket-name)
-    (sb-ext:run-program "rm" (list "-f" socket-name)
-                        :search t :wait t)
-    (sb-ext:run-program "chmod" (list "u=rwx" "og=" socket-container)
-                        :search t :wait t)
-    (format *trace-output* "Socket FS preparations done~%")
-    (sb-bsd-sockets:socket-bind socket socket-name)
-    (sb-bsd-sockets:socket-listen socket 3)
-    (format *trace-output* "Socket created~%")
-    (sb-thread:make-thread
-      (lambda ()
-        (loop
-          for connection := (sb-bsd-sockets:socket-accept socket)
-          for stream := (sb-bsd-sockets:socket-make-stream
-                          connection
-                          :buffering :none
-                          :input t :output t
-                          :external-format :utf8)
-          do (format *trace-output* "Accepted a socket connection~%")
-          do (sb-thread:make-thread
-               (let
-                 ((stream stream))
-                 (lambda ()
-                   (unwind-protect
-                     (let 
-                       ((*package* (find-package :stumpwm)))
-                       (loop
-                         for form := (ignore-errors (read stream nil nil))
-                         for dummy := 
-                         (format *error-output* "Got form to evaluate:~%~s~%"
-                                 form)
-                         while (and form (not (eq form :exit)))
-                         for result := (multiple-value-bind (result error)
-                                         (ignore-errors
-                                           (list (eval form)))
-                                         (if result 
-                                           (first result)
-                                           (format nil "Error:~%~a" error)))
-                         do (format *error-output* "Evaluated form to:~%~S~%"
-                                    result)
-                         do (format stream "~s~%" result)
-                         do (finish-output stream)))
-                     (ignore-errors (close stream)))))
-                 :name "StumpWM socket connection handler")))
-      :name "StumpWM socket evaluator")))
+(defcommand 
+  tag-frame-windows (tags) ((:rest "Tags to add: "))
+  (act-on-matching-windows
+    (w :frame) t
+    (setf (window-tags w)
+          (union
+            (window-tags w)
+            (windowtags::string-split-by-spaces tags)
+            :test 'equalp))))
+
+(defcommand 
+  untag-frame-windows (tags) ((:rest "Tags to remove: "))
+  (act-on-matching-windows
+    (w :frame) t
+    (setf (window-tags w)
+          (set-difference
+            (window-tags w)
+            (windowtags::string-split-by-spaces tags)
+            :test 'equalp))))
+
+(defcommand 
+  tag-frame-windows-only (tags) ((:rest "Tags to set: "))
+  (act-on-matching-windows
+    (w :frame) t
+    (setf (window-tags w)
+          (windowtags::string-split-by-spaces tags))))
+
+(defcommand 
+  tag-frame-windows-no-auto (tags) ((:rest "Tags to set: "))
+  (act-on-matching-windows
+    (w :frame) t
+    (setf (window-tags w)
+          (union
+            (list "NO-AUTO-TAGS")
+            (windowtags::string-split-by-spaces tags)
+            :test 'equalp))))
+
+(defcommand rerun-start-hook () ()
+            (run-hook *start-hook*))
