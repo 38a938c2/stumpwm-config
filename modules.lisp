b802b3b3256e5bb4bea77ba904c4a976b715da3d
Michael Raskin <38a938c2@rambler.ru>
2023-03-31 02:47:23 +0200

    Syncing from Monotone at 20230331-024723. Last commit message was Try to make sure everything is loaded

--- modules.lisp
+++ modules.lisp
@@ -1,5 +1,8 @@
 (in-package :stumpwm)
 
+(loop for x in `(:clx-truetype :xkeyboard :xembed)
+      do (require x))
+
 (defparameter *local-module-dir*  "/home/repos/stumpwm-contrib/")
 (set-module-dir *local-module-dir*)
 (load-module "windowtags")
@@ -7,3 +10,6 @@
 (use-package :windowtags)
 (import 'windowtags::select-by-tags)
 
+
+(loop for x in `(:clx-truetype :xkeyboard :xembed :windowtags :ttf-fonts)
+      do (require x))
